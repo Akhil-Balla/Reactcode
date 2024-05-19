7445d922c372f1a3fbd3cd85be588533e0f09760
Akhil Balla <akhilballa761996@gmail.com>
2024-05-19 15:53:22 +0530

    Second commit

---
+++ src/components/login/signup/Signup.tsx
@@ -0,0 +1,14 @@
+import { Grid } from '@mui/material';
+
+const Signup = () => {
+  return (
+    <Grid>
+        <Grid className="Secondgrid">
+            <h1>Akhil</h1>
+        </Grid>
+        <Grid></Grid>
+    </Grid>
+  )
+}
+
+export default Signup
